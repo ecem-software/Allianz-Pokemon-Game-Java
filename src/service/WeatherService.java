bb3a40b6771cd76502a1ca53dab0d9db3e06c9c3
Ecem Terzi <ecemterz@gmail.com>
2023-07-28 10:12:25 +0300

    Game project is ready.

--- src/service/WeatherService.java
+++ src/service/WeatherService.java
@@ -6,28 +6,11 @@ import java.util.Random;
 
 public class WeatherService {
 
-    PokemonService pokemonService = new PokemonService();
-
     public WeatherConditionEnum randomWeather() {
         int pickWeather = new Random().nextInt(WeatherConditionEnum.values().length);
         WeatherConditionEnum randomWeather = WeatherConditionEnum.values()[pickWeather];
         return randomWeather;
     }
-//    public void createWeather(){
-//        WeatherConditionEnum randomWeather = randomWeather();
-//
-//            if (randomWeather == WeatherConditionEnum.SUNNY) {
-//                System.out.println("The day is Sunny., Pikachu's electricty can be affected bad from that weather condition.");
-//
-//            } else if (randomWeather == WeatherConditionEnum.RAINY) {
-//                System.out.println("The day is Rainy.");
-//            } else if (randomWeather == WeatherConditionEnum.CLOUDY) {
-//                System.out.println("The day is Cloudy.");
-//            } else if (randomWeather == WeatherConditionEnum.SNOWY) {
-//                System.out.println("The day is Snowy.");
-//            }
-//    }
-
 
 }
 
